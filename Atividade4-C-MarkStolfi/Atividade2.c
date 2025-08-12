c9135aef4e9bebbae8f48b7c01264adad300d313
MarkStolfi <markstolfi27@gmail.com>
2025-08-12 14:10:46 -0300

    ARRUMEI O BUG, (era obvio)

--- Atividade4-C-MarkStolfi/Atividade2.c
+++ Atividade4-C-MarkStolfi/Atividade2.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <limits.h>
 int main() {
-    int codigo_maior=0, codigo_menor=0, acidente=0, maior_ind=0, menor_ind=INT_MAX, veiculo;
+    int ind_maior=0, ind_menor=0, acidente=0, maior_ind=0, menor_ind=INT_MAX, veiculo;
     float media_acidente = 0, media_veiculo = 0;
 
 
@@ -19,24 +19,19 @@ int main() {
         if (veiculo <2000){
             media_acidente += acidente;
         }
-
-        if (acidente < menor_ind){
-            menor_ind = acidente;
-        }
-
         if (acidente > maior_ind){
             maior_ind = acidente;
-            codigo_maior = i;
+            ind_maior = i;
         }
-        if (acidente < menor_ind) {
+        if (acidente < menor_ind){
             menor_ind = acidente;
-            codigo_menor = i;
+            ind_menor = i;
         }
 
 
     }
-printf("\nMaior Indice: %d Pertence a cidade %d\n", maior_ind, codigo_maior);
-printf("\nMenor Indice: %d Pertence a cidade %d\n", menor_ind, codigo_menor);
+printf("\nMaior Indice: %d | Pertence a cidade: %d\n", maior_ind, ind_maior);
+printf("\nMenor Indice: %d | Pertence a cidade: %d\n", menor_ind, ind_menor);
 printf("\nMedia de veiculos nas 5 cidades: %.0f\n", media_veiculo/5);
 printf("\nMedia de acidentes nas 5 cidades: %.0f\n", media_acidente/5);
 return 0;
