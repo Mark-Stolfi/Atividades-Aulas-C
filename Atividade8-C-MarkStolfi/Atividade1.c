97248a83c15a602b39b6b086f68a0f56e7adfda8
MarkStolfi <markstolfi27@gmail.com>
2025-08-13 16:42:39 -0300

    .

---
+++ Atividade8-C-MarkStolfi/Atividade1.c
@@ -0,0 +1,30 @@
+//
+// Created by mark_stolf on 13/08/2025.
+//
+#include <stdio.h>
+int main() {
+int m[2][2], r[2][2], maior=0;
+
+    printf("Digite 4 numeros\n");
+    for (int l = 0; l < 2; l++) {
+        for (int c = 0; c < 2; c++) {
+            printf("\nLinha %d Coluna %d = ", l, c);
+            scanf("%d", &m[l][c]);
+            if (m[l][c] > maior) {
+                maior = m[l][c];
+            }
+        }
+    }
+
+    for (int l = 0; l < 2; l++) {
+        for (int c = 0; c < 2; c++) {
+            r[l][c] = m[l][c];
+            if (r[l][c] < maior) {
+                r[l][c] = r[l][c] * maior;
+            }
+            printf("%d | ", r[l][c]);
+
+        }
+        printf("\n");
+    }
+}
