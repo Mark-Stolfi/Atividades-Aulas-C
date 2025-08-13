d17eded3408194044977901646d52206dca36870
MarkStolfi <markstolfi27@gmail.com>
2025-08-13 16:44:48 -0300

    Update Atividade1.c

--- Atividade8-C-MarkStolfi/Atividade1.c
+++ Atividade8-C-MarkStolfi/Atividade1.c
@@ -8,7 +8,9 @@ int m[2][2], r[2][2], maior=0;
     printf("Digite 4 numeros\n");
     for (int l = 0; l < 2; l++) {
         for (int c = 0; c < 2; c++) {
+
             printf("\nLinha %d Coluna %d = ", l, c);
+
             scanf("%d", &m[l][c]);
             if (m[l][c] > maior) {
                 maior = m[l][c];
@@ -20,7 +22,8 @@ int m[2][2], r[2][2], maior=0;
         for (int c = 0; c < 2; c++) {
             r[l][c] = m[l][c];
             if (r[l][c] < maior) {
-                r[l][c] = r[l][c] * maior;
+                r[l][c] = r[l][c] * maior;//Entendi que o maior elemento (multiplicador)
+                                          //não seja multiplicado por ele mesmo
             }
             printf("%d | ", r[l][c]);
 
